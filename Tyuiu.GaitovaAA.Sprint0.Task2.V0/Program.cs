91ac8091b3526b8c4dc373118d6395ca64e8bf96
Arina Gaitova <arinagaitova@gmail.com>
2026-09-13 01:17:25 +0500

    Task2

---
+++ Tyuiu.GaitovaAA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.GaitovaAA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.GaitovaAA.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вызов библиотеки DataService и метода Getmessage
+            //из библиотеки Tyuiu.GaitovaAA.Sprint0.Task2.V0.Lib
+            Console.WriteLine(DataService.GetMessage("Арина"));
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
+}
